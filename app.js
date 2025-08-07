ba5bf51a320226cf4262345348649948a20e1518
duridev <duridev@outlook.com>
2025-08-06 22:53:17 -0400

    creacion funcion agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+
+let agregarAmigo = () => {
+    let amigo = document.querySelector('#amigo').trim();
+    if(amigo = ""){
+        alert("El campo Nombre no puede ir vacío")
+        return
+    }else{
+        listaAmigos.push(amigo);
+    }
+};
+
