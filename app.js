e5bcb57a38dcf90396716bf968bb54839c6bdcf4
duridev <duridev@outlook.com>
2025-08-06 23:16:48 -0400

    creacion funcion mostrarAmigo()

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
-
+let arrayAmigos = [];
+let amigo = document.querySelector('#amigo').value;
+let listaAmigos = document.querySelector('#listaAmigos');
 
 let agregarAmigo = () => {
-    let amigo = document.querySelector('#amigo').trim();
     if(amigo = ""){
         alert("El campo Nombre no puede ir vacío")
         return
     }else{
-        listaAmigos.push(amigo);
+        arrayAmigos.push(amigo);
     }
 };
 
+let mostrarAmigos = () => {
+    for(let i = 0; i < arrayAmigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${arrayAmigos[i]}</li>`
+    };
+};
+
+mostrarAmigos();
