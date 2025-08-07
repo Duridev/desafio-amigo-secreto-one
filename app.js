0c3c303610ff56673f4d29f5b68ceee8504f29cd
duridev <duridev@outlook.com>
2025-08-07 00:30:59 -0400

    funcion indice aleatorio

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let arrayAmigos = [];
-let amigo = document.querySelector('#amigo').value;
 let listaAmigos = document.querySelector('#listaAmigos');
 
 let agregarAmigo = () => {
-    if(amigo = ""){
+    let amigo = document.querySelector('#amigo').value;
+    if(amigo.trim() == ""){
         alert("El campo Nombre no puede ir vacío")
-        return
     }else{
-        arrayAmigos.push(amigo);
+        arrayAmigos.push(amigo.trim());
+        document.querySelector('#amigo').value = ""
+        console.log(arrayAmigos);
+        mostrarAmigos();
     }
 };
 
 let mostrarAmigos = () => {
+     listaAmigos.innerHTML = "";
     for(let i = 0; i < arrayAmigos.length; i++) {
         listaAmigos.innerHTML += `<li>${arrayAmigos[i]}</li>`
     };
 };
 
 mostrarAmigos();
+
+let indiceAlAzar = () => {
+    let indiceAleatorio = Math.floor(Math.random() * (arrayAmigos.length))
+    return indiceAleatorio
+}
+
+sortearAmigo
