0ea960f3fa46cee4cbffdba20d1265ac72103f81
duridev <duridev@outlook.com>
2025-08-07 20:40:04 -0400

    Finalizacion del proyecto

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let arrayAmigos = [];
 let listaAmigos = document.querySelector('#listaAmigos');
+let resultado = document.querySelector('#resultado');
+
+
+let mostrarAmigos = () => {
+    listaAmigos.innerHTML = "";
+    for(let i = 0; i < arrayAmigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${arrayAmigos[i]}</li>`
+    };
+};
 
 let agregarAmigo = () => {
     let amigo = document.querySelector('#amigo').value;
@@ -11,21 +20,24 @@ let agregarAmigo = () => {
         document.querySelector('#amigo').value = ""
         console.log(arrayAmigos);
         mostrarAmigos();
+        resultado.innerHTML = ""
     }
 };
 
-let mostrarAmigos = () => {
-     listaAmigos.innerHTML = "";
-    for(let i = 0; i < arrayAmigos.length; i++) {
-        listaAmigos.innerHTML += `<li>${arrayAmigos[i]}</li>`
-    };
-};
-
-mostrarAmigos();
 
 let indiceAlAzar = () => {
     let indiceAleatorio = Math.floor(Math.random() * (arrayAmigos.length))
     return indiceAleatorio
 }
 
-sortearAmigo
+let sortearAmigo = () => {
+    if(arrayAmigos.length <= 1) {
+        alert("Ingresa más nombres para comenzar el sorteo.")
+    } else {
+        indiceAlAzar();
+        let ganador = arrayAmigos[indiceAlAzar()];
+        resultado.innerHTML = ganador
+        arrayAmigos = [];
+        listaAmigos.innerHTML = "";
+    }
+}
